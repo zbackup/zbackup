9709e1cdb8e6d4b9f60bd8194ce8e7d245878a71
Konstantin Isakov <dragonroot@gmail.com>
2014-10-10 13:59:21 +0400

    Release version 1.3.

--- zbackup.cc
+++ zbackup.cc
@@ -372,7 +372,7 @@ int main( int argc, char *argv[] )
     if ( args.size() < 1 )
     {
       fprintf( stderr,
-"ZBackup, a versatile deduplicating backup tool, version 1.2\n"
+"ZBackup, a versatile deduplicating backup tool, version 1.3\n"
 "Copyright (c) 2012-2014 Konstantin Isakov <ikm@zbackup.org>\n"
 "Comes with no warranty. Licensed under GNU GPLv2 or later + OpenSSL.\n"
 "Visit the project's home page at http://zbackup.org/\n\n"
