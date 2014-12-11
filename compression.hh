7658ac10c02981e1d5f8f6138f61b7f3d9f2f4fc
Am1GO <amigo.elite@gmail.com>
2014-12-11 10:54:58 +0300

    Source headers altered

--- compression.hh
+++ compression.hh
@@ -1,4 +1,3 @@
-// Copyright (c) 2013 Benjamin Koch <bbbsnowball@gmail.com>
 // Copyright (c) 2012-2014 Konstantin Isakov <ikm@zbackup.org> and ZBackup contributors, see CONTRIBUTORS
 // Part of ZBackup. Licensed under GNU GPLv2 or later + OpenSSL, see LICENSE
 
