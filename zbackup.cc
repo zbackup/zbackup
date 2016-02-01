38cee26cee4308c00497e200ca3dcd56724e6e57
Vladimir Rutsky <vladimir@rutsky.org>
2016-02-01 04:39:09 +0300

    implement POC NBD server in zbackup
    Allows to serve backup contents as block device.
    Useful if backup is an disk partition image.
    
    If "/repo/backups/sda1-2016-02-01" is an "/dev/sda1" partition backup,
    then you can do:
    
    # Load Linux NBD driver
    sudo modprobe nbd
    # Start serving backup as /dev/nbd0
    sudo zbackup --non-encrypted nbd /repo/backups/sda1-2016-02-01 /dev/nbd0 &
    # Mount NBD as real read-only filesystem
    sudo mount -o ro /dev/nbd0 /mnt
    
    Tested on Ubuntu 14.04 64-bit with backup of 80 GB NTFS partition.
    
    This is a quickly done Proof of Concept implementation.
    It's not heavily tested and not profiled (however I haven't
    noticed any significant filesystem lags).
    
    Later NBD code can be extracted to BUSE and BUSE can be dropped completely.

--- zbackup.cc
+++ zbackup.cc
@@ -173,6 +173,8 @@ invalid_option:
 "    restore <backup file name> - restores a backup to stdout\n"
 "    restore <backup file name> <output file name> - restores\n"
 "            a backup to file using two-pass \"cacheless\" process\n"
+"    nbd <backup file name> /dev/nbd0\n"
+"            start NBD server that will serve backup data as block device\n"
 "    export <source storage path> <destination storage path> -\n"
 "            performs export from source to destination storage\n"
 "    import <source storage path> <destination storage path> -\n"
@@ -276,6 +278,20 @@ invalid_option:
         zr.restoreToFile( args[ 1 ], args[ 2 ] );
       else
         zr.restoreToStdin( args[ 1 ] );
+    }
+    else
+    if ( strcmp( args[ 0 ], "nbd" ) == 0 )
+    {
+      // Start NBD server
+      if ( args.size() != 3 )
+      {
+        fprintf( stderr, "Usage: %s %s <backup file name> /dev/nbd0\n",
+                 *argv , args[ 0 ] );
+        return EXIT_FAILURE;
+      }
+      ZRestore zr( ZRestore::deriveStorageDirFromBackupsFile( args[ 1 ] ),
+                   passwords[ 0 ], config );
+      zr.startNBDServer( args[ 1 ], args[ 2 ] );
     }
     else
     if ( strcmp( args[ 0 ], "export" ) == 0 || strcmp( args[ 0 ], "import" ) == 0 )
